b359b512f2be49e51019060598301d556666e968
IanGustavo15 <98957131+IanGustavo15@users.noreply.github.com>
2025-11-21 10:34:23 -0300

    corrigindo shadcn para o dashboard

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -4,9 +4,10 @@ import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
-import { VisGroupedBar, VisXYContainer } from '@unovis/vue';
-// import type { ChartConfig } from '@/registry/new-york-v4/ui/chart';
-// import { ChartContainer } from '@/registry/new-york-v4/ui/chart';
+import type { ChartConfig } from '@/components/ui/chart';
+import { Monitor } from 'lucide-vue-next';
+import { VisAxis, VisGroupedBar, VisXYContainer } from '@unovis/vue';
+import { ChartContainer, ChartCrosshair, ChartLegendContent, ChartTooltip, ChartTooltipContent, componentToString } from '@/components/ui/chart';
 
 const chartData = [
   { date: new Date('2024-01-01'), desktop: 186, mobile: 80 },
@@ -20,11 +21,11 @@ type Data = typeof chartData[number]
 const chartConfig = {
   desktop: {
     label: 'Desktop',
-    color: '#2563eb',
+    color: '#F90D0D',
   },
   mobile: {
     label: 'Mobile',
-    color: '#60a5fa',
+    color: '#57F77A',
   },
 } satisfies ChartConfig
 
@@ -47,19 +48,7 @@ const breadcrumbs: BreadcrumbItem[] = [
                 <div
                     class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
                 >
-                    <ChartContainer :config="chartConfig" class="min-h-[200px] w-full">
-    <VisXYContainer :data="chartData">
-      <VisGroupedBar
-        :x="(d: Data) => d.date"
-        :y="[(d: Data) => d.desktop, (d: Data) => d.mobile]"
-        :color="[chartConfig.desktop.color, chartConfig.mobile.color]"
-        :rounded-corners="4"
-        bar-padding="0.1"
-        group-padding="0"
-      />
-    </VisXYContainer>
-  </ChartContainer>
-
+                    <PlaceholderPattern />
                 </div>
                 <div
                     class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
@@ -75,7 +64,39 @@ const breadcrumbs: BreadcrumbItem[] = [
             <div
                 class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border"
             >
-                <PlaceholderPattern />
+                <ChartContainer :config="chartConfig" class="min-h-[200px] w-full">
+    <VisXYContainer :data="chartData">
+      <VisGroupedBar
+        :x="(d: Data) => d.date"
+        :y="[(d: Data) => d.desktop, (d: Data) => d.mobile]"
+        :color="[chartConfig.desktop.color, chartConfig.mobile.color]"
+        :rounded-corners="4"
+        bar-padding="0.1"
+        group-padding="0"
+      />
+      <VisAxis
+        type="x"
+        :x="(d: Data) => d.date"
+        :tick-line="false"
+        :domain-line="false"
+        :grid-line="false"
+        :tick-format="(d: number) => {
+          const date = new Date(d)
+          return date.toLocaleDateString('en-US', {
+            month: 'short',
+          })
+        }"
+        :tick-values="chartData.map(d => d.date)"
+      />
+      <VisAxis
+        type="y"
+        :tick-format="(d: number) => ''"
+        :tick-line="false"
+        :domain-line="false"
+        :grid-line="true"
+      />
+    </VisXYContainer>
+  </ChartContainer>
             </div>
         </div>
     </AppLayout>
